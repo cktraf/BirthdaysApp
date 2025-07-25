24cca0b5bc4b9d65fddaff952016c1ffdabb907a
Carissa Traffie <carissa@traffie.com>
2025-07-25 09:57:15 -0700

    Initial Commit

---
+++ BirthdaysApp/BirthdaysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysAppApp.swift
+//  BirthdaysApp
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
